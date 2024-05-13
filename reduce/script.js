b2b9aeaf75446175752a6ba8de9736ed7b35012e
jayak <kumarijaya127@gmail.com>
2024-05-13 22:18:10 +0530

    add reduce

--- reduce/script.js
+++ reduce/script.js
@@ -1,9 +1,29 @@
 const number = [1, 2, 3, 4, 5, 6];
 
-const num = 0;
+const num = 5;
 const sumFun = number.reduce((accumulator, currentValue) => {
   console.log(`accumulator: ${accumulator} and currentValue: ${currentValue}`);
   return accumulator + currentValue;
 }, num);
 
 console.log(sumFun);
+
+// shopping cart
+
+const shoppingCart = [
+  {
+    itemName: "javascript",
+    price: 3000,
+  },
+  {
+    itemName: "python",
+    price: 2999,
+  },
+  {
+    itemName: "reactjs",
+    price: 4999,
+  },
+];
+
+const priceToPay = shoppingCart.reduce((accu, item) => accu + item.price, 0);
+console.log(priceToPay);
